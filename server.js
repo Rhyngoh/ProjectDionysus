03438e76065dbcfa624d79b55a9517185a94e0b0
michaeledwards91 <michael.edwards1991@gmail.com>
2016-12-16 10:10:29 -0600

    Setup server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+//Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-
+//Express setup
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+//Body Parser setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
+//Set static folder to public
 app.use(express.static("./public"));
+//Require routes
+require("./controllers/html-routes.js")(app);
+//Handlebars setup
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-require("./routes/html-routes.js")(app);
+app.listen(PORT, function() {
+	console.log("Server listening on port " + PORT);
+});
