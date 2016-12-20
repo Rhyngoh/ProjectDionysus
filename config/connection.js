a78385fb5149e98e19f17eaa699bfe5d5bcbb906
Steve Chronister <Steve Chronister>
2016-12-20 12:46:32 -0600

    added sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,15 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+//connection to sequelize
+var Sequelize = require("sequelize");
 
-var connection 
-
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "shoppinglistdb"
-  });
-}
-
-
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+//create mySQL connection
+var sequelize = new Sequelize("shoppinglistdb", "root", "root", {
+  host:"localhost",
+  dialect:"mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-  console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+module.exports = sequelize;
---
+++ models/recipe.js
@@ -0,0 +1,41 @@
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
+
+//creating the recipe model
+var Recipe = sequelize.define("recipe", {
+    id: {
+        type:Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    recipe_name:{
+        type: Sequelize.STRING
+    },
+    recipe_url:{
+        type: Sequelize.STRING
+
+    },
+    recipe_image:{
+        type: Sequelize.STRING
+
+    },
+    ingredients:{
+        type:Sequelize.STRING
+    },
+    raw_ingredients:{
+        type:Sequelize.STRING
+    },
+    instructions:{
+        type:Sequelize.STRING
+    },
+    createdAt:{
+        type:Sequelize.DATE
+    }
+}, {
+    timestamps: false
+});
+
+//sync with db
+Recipe.sync();
+
+module.exports = Recipe;
