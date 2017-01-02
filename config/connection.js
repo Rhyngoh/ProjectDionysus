8c714d07b7b5d9f660fd0f5d89133b0a1fe8093b
Ryan Ngoh <rxn120230@utdallas.edu>
2017-01-02 15:20:03 -0600

    configured connection.js to have jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,26 @@
 //connection to sequelize
 var Sequelize = require("sequelize");
-
+var sequelize = null;
 //create mySQL connection
-var sequelize = new Sequelize("shoppinglistdb", "root", "root", {
-  host:"localhost",
-  dialect:"mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+if(process.env.JAWSDB_URL){
+	sequelize = new Sequelize(process.env.JAWSDB_URL, {
+		dialect:"mysql",
+	    pool: {
+	      max: 5,
+	      min: 0,
+	      idle: 10000
+	    }
+	});
+}else{
+	sequelize = new Sequelize("shoppinglistdb", "root", "root", {
+	    host:"localhost",
+	    dialect:"mysql",
+	    pool: {
+	      max: 5,
+	      min: 0,
+	      idle: 10000
+	    }
+	});
+}
 
 module.exports = sequelize;
