6382e645f5c74f90bf17608253c6386e2ed0c789
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-01-02 22:35:40 -0600

    auto complete working

--- public/assets/javascript/frontend.js
+++ public/assets/javascript/frontend.js
@@ -1,11 +1,26 @@
-//autocomplete for cuisine search
-// $('input.autocomplete').autocomplete({
-//    data: {
-//      "Apple": null,
-//      "Microsoft": null,
-//      "Google": 'http://placehold.it/250x250'
-//    }
-//  });
+var autoObj = new Object();
+//console.log(autoObj);
+var count = 0;
+
+$.get("/ingredients", function(ing){
+	for(i in ing){
+
+		autoObj[ing[i].ingredient_name] = null;
+		
+		
+	}
+	console.log(autoObj);
+
+	
+
+	$('input.autocomplete').autocomplete({
+	//loop to itterate over all raw ingredients 
+
+   data: autoObj
+ });
+
+});
+
 
  // parallax
 
