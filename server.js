c96e93e3b72c6336d311d64d9acfc0a05cd6c14c
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-02 23:21:27 -0600

    Browse page showing some recipe cards on load

--- server.js
+++ server.js
@@ -2,26 +2,33 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+
 //Express setup
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 //Body Parser setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 //Set static folder to public
 app.use(express.static("./public"));
+
 //Method Override setup
 app.use(methodOverride("_method"));
+
 //Require routes
 require("./controllers/html-routes.js")(app);
 require("./controllers/api-routes.js")(app);
+
 //Handlebars setup
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//Require our models
 var db = require("./models");
 
 
